6f12c4c48c8a369409412d1ffedb269e60893554
Valentin <1032202474@pfur.ru>
2024-05-03 17:31:56 +0300

    handle bad cases

--- frontend/script.js
+++ frontend/script.js
@@ -1,11 +1,11 @@
-// script.js
-async function getRepos() {
-  const url = 'http://localhost:8080/get_repos'
+async function getRepos(organization, token) {
+  const organizationValue = organization.value;
+  const tokenValue = token.value;
+  const url = `http://localhost:8080/get_repos_with_params?token=${tokenValue}&link=${organizationValue}`;
   try {
     const response = await fetch(url);
     const data = await response.json();
     handleData(data)
-    //handleResponse(response.status, data); // Pass status code and parsed response body to handleResponse function
   } catch (error) {
     console.error('Error fetching data:', error);
   }
@@ -14,20 +14,27 @@ async function getRepos() {
 function handleData(data) {
   console.log(data);
   const resultContainer = document.getElementById('resultContainer');
-  resultContainer.innerHTML = ''; // Clear previous content
-  const repositories = data.body;
-  for (const repo of Object.values(repositories)) {
-    const repoName = repo.name;
-    const hasHello = repo.hasHello;
+  resultContainer.innerHTML = '';
+  console.log(data.statusCode)
+  if (data.statusCode === 200) {
+    const repositories = data.body;
+    for (const repo of Object.values(repositories)) {
+      const repoName = repo.name;
+      const hasHello = repo.hasHello;
 
-    const repoElement = document.createElement('p');
-    repoElement.textContent = repoName;
+      const repoElement = document.createElement('p');
+      repoElement.textContent = repoName;
 
-    if (hasHello) {
-      repoElement.style.color = 'red';
+      if (hasHello) {
+        repoElement.style.color = 'red';
+      }
+
+      resultContainer.appendChild(repoElement);
     }
-
-    resultContainer.appendChild(repoElement);
+  } else {
+    const errorMessage = document.createElement('p');
+    errorMessage.textContent = data.message;
+    resultContainer.appendChild(errorMessage);
   }
 
 }
@@ -39,6 +46,6 @@ document.addEventListener('DOMContentLoaded', function () {
 
   goButton.addEventListener('click', function () {
     console.log("pressed")
-    getRepos()
+    getRepos(organization, token)
   });
 });
